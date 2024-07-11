c06941723e0bd3f09f82b461eecc1fd6d4381c47
Wuillaume Maria <maria.wuillaume@nokia.com>
2024-07-11 11:07:09 -0300

    update genetic algorithm

--- genetic_algorithm/genetic_algorithm_net.py
+++ genetic_algorithm/genetic_algorithm_net.py
@@ -2,8 +2,10 @@
 from sklearn.metrics import r2_score
 import copy
 import operator
+from tensorflow.keras.losses import BinaryCrossentropy, BinaryFocalCrossentropy
 import random
 import pandas as pd
+import keras.metrics as metrics
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -29,19 +31,18 @@ class architecture:
         self.neurons = neurons
         self.activation = activation
         self.optimizer = optimizer
-    def __deepcopy__(self, memo):
-        from copy import deepcopy
-        new_copy = type(self)(deepcopy(self.some_data, memo))
-        return new_copy
 
 
-
-class GA_assymetric_autoencoder:
-    def __init__(self, shape, coding_size, X_train, X_test, DNA_parameter, epochs):
-        self.shape = shape
+class GA:
+    def __init__(self, coding_size, X_train, X_test, y_train, y_test, DNA_parameter, epochs):
+        self.shape = X_train.shape[1]
+        self.target_shape = len(y_train.unique())
         self.code_size = coding_size
         self.X_train = X_train
         self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
+        
         self.activations = DNA_parameter[0]
         self.optimizers = DNA_parameter[1]
         self.epochs = epochs
@@ -49,7 +50,7 @@ class GA_assymetric_autoencoder:
     
     #This one shows the real numbers of neurons instead of percentage
     
-    def build_autoencoder(self, architecture, output='decoder'):
+    def build_neural_net(self, architecture, output='decoder'):
         decoder_arch = architecture['decoder']
         encoder_arch = architecture['encoder']
         code = architecture['code'][0]
@@ -79,7 +80,13 @@ class GA_assymetric_autoencoder:
         elif output ==  'encoder':
             autoencoder = Model(input_layer, encoder_output)
         # Compile the model
-        autoencoder.compile(optimizer=decoder_arch[-1].optimizer, loss='mean_squared_error')
+        
+        loss = BinaryFocalCrossentropy(gamma=2.0, alpha=0.25)
+        autoencoder.compile(optimizer=decoder_arch[-1].optimizer, loss=loss, metrics=[
+            'accuracy',
+            metrics.Recall(name='recall'),
+            metrics.Precision(name='precision')
+        ])
 
         return autoencoder
     
@@ -92,21 +99,25 @@ class GA_assymetric_autoencoder:
         print('-'*30)
         
     def train(self, model):
-        model.fit(self.X_train, self.X_train, 
+        model.fit(self.X_train, self.y_train, 
             epochs= self.epochs, 
             verbose=0,
-            batch_size=32,
-            shuffle=True)
-        decoded_data = model.predict(self.X_test)
-        global_r2 = r2_score(self.X_test, decoded_data)
-        return global_r2, model
+            batch_size=32, validation_split=0.2)
+        
+        # Evaluate the model
+        loss, accuracy, precision, recall = model.evaluate(self.X_test, self.y_test)
+        if precision > 0 and recall > 0:
+            f1 = (2*precision*recall)/(precision+recall)
+        else:
+            f1 = 0
+        return accuracy, f1, precision, recall, model
     
     def create_population(
         self, 
         hidden_layers_encoder, 
         hidden_layers_decoder, 
+        max_dim_increase,
         population_size=20, 
-        max_dim_increase=60
         ):
         architectures=[]
 
@@ -119,8 +130,7 @@ class GA_assymetric_autoencoder:
             the_architecture_decoder = []
             
             neurons_encoder = [max_dim_increase]
-            neurons_decoder = [self.shape]
-            #We only need to create and optimize architecture for half of the depth of the network because we want to create symmetric autoencoders
+    
             the_architecture_encoder.append(architecture(neurons=self.shape)) # Input
             
             for i in range(hidden_layers_encoder):
@@ -134,19 +144,9 @@ class GA_assymetric_autoencoder:
                 
             code_layer = architecture(neurons=self.code_size, activation=np.random.choice(self.activations)) # Latent dimension
             
-            
-            decoder_list = []
-            for i in range(hidden_layers_decoder):
-                activation  = np.random.choice(self.activations)
-                ch_neuron =  np.random.rand()
-                units = round(neurons_decoder[i]*ch_neuron)
-                if units==0 or units<=self.code_size:
-                    break
-                neurons_decoder.append(units)
-                decoder_list.append(architecture(neurons=units,activation=activation))
-            
-            the_architecture_decoder = decoder_list[::-1]
-            the_architecture_decoder.append(architecture(neurons=self.shape, activation=np.random.choice(self.activations), optimizer=np.random.choice(self.optimizers)))
+
+        
+            the_architecture_decoder.append(architecture(neurons=1, activation='sigmoid', optimizer=np.random.choice(self.optimizers)))
             architectures.append({
             'encoder': the_architecture_encoder,
             'code': [code_layer],
@@ -175,8 +175,6 @@ class GA_assymetric_autoencoder:
     def recombine(self, ae1, ae2):
         encoder1 = ae1['encoder'][1:]
         encoder2 = ae2['encoder'][1:]
-        decoder1 = ae1['decoder'][:-1]
-        decoder2 = ae2['decoder'][:-1]
         # Selecionar pontos de corte para o encoder e o decoder
         if len(encoder1) == 0:
             cut_point_encoder1 = 0
@@ -187,33 +185,17 @@ class GA_assymetric_autoencoder:
             cut_point_encoder2 = 0
         else:
             cut_point_encoder2 = np.random.randint(0, len(encoder2)+1)
-        
-        if len(decoder1) == 0:
-            cut_point_decoder1 = 0 
-        else:
-            cut_point_decoder1 = np.random.randint(0, len(decoder1)+1)
-            
-        if len(decoder2) == 0:
-            cut_point_decoder2 = 0
-        else:
-            cut_point_decoder2 = np.random.randint(0, len(decoder2)+1)
-        
         # Recombinar os encoders e decoders
 
         new_encoder = encoder1[:cut_point_encoder1] + encoder2[cut_point_encoder2:]
-        new_decoder = decoder2[:cut_point_decoder2] + decoder1[cut_point_decoder1:]
-
         # Verificar e corrigir se necessário
         if not self._valid_encoder(new_encoder, self.code_size):
             new_encoder = sorted(new_encoder, key=lambda x: x.neurons, reverse=True)
-        if not self._valid_decoder(new_decoder, self.shape, self.code_size):
-            new_decoder = sorted(new_decoder, key=lambda x: x.neurons)
-
         # Retornar os novos encoder e decoder recombinados
         return {
             'encoder': [ae1['encoder'][0]] + new_encoder,
             'code': ae1['code'],
-            'decoder':   new_decoder + [ae1['decoder'][-1]]
+            'decoder':  ae1['decoder']
         }
         
     def mutate(self, ae, max_try=100):
@@ -221,43 +203,36 @@ class GA_assymetric_autoencoder:
             # Gerar fator de mutação
             max_try -= 1
             encoder = ae['encoder'][1:]
-        
-            decoder = ae['decoder'][:-1]
-            mutation_factor_encoder = np.random.rand() + np.random.randint(0, 2)
-            mutation_factor_decoder = np.random.rand() + np.random.randint(0, 2)
-            
-            new_encoder = []
-            new_decoder = []
             
             
-            
-            # Aplicar mutação
-            for layer in encoder:
-                new_layer = copy.copy(layer)
-                new_layer.neurons = round(layer.neurons * mutation_factor_encoder)
-                new_layer.activation = np.random.choice(self.activations)
-                new_encoder.append(new_layer)
-            for layer in decoder:
-                new_layer = copy.copy(layer)
-                new_layer.neurons = round(layer.neurons * mutation_factor_decoder)
-                new_layer.activation = np.random.choice(self.activations)
-                new_decoder.append(new_layer)
+            if len(encoder) == 0:
+                return {
+                    'encoder': [ae['encoder'][0]],
+                    'code': ae['code'],
+                    'decoder':  ae['decoder']
+                }
 
+            mutation_factor_encoder = np.random.rand() + np.random.randint(0, 2)
+            
+            new_encoder = copy.deepcopy(encoder)
+            
+            new_layer = np.random.choice(new_encoder)
+            new_layer.neurons = round(new_layer.neurons * mutation_factor_encoder)
+            new_layer.activation = np.random.choice(self.activations)
+            
+        
             # Verificar se o encoder é válido
             if not self._valid_encoder(new_encoder, self.code_size):
                 continue
             
-            # Verificar se o decoder é válido
-            if not self._valid_decoder(new_decoder, self.shape, self.code_size):
-                continue
-            
+
             ae['decoder'][-1].optimizer = np.random.choice(self.optimizers)
             
             # Se ambos são válidos, retornar os mutados
             return {
                 'encoder': [ae['encoder'][0]] + new_encoder,
                 'code': ae['code'],
-                'decoder':   new_decoder + [ae['decoder'][-1]]
+                'decoder':  ae['decoder']
             }
         return ae
         
@@ -279,9 +254,9 @@ class GA_assymetric_autoencoder:
             P3= population[sel_index[2]]
 
             
-            P1_model = self.build_autoencoder(P1)
-            P2_model = self.build_autoencoder(P2)
-            P3_model = self.build_autoencoder(P3)
+            P1_model = self.build_neural_net(P1)
+            P2_model = self.build_neural_net(P2)
+            P3_model = self.build_neural_net(P3)
 
             P1_model_trained = self.train(P1_model)
             P2_model_trained = self.train(P2_model)
@@ -292,25 +267,34 @@ class GA_assymetric_autoencoder:
                 {
                     'p': P1,
                     'i': sel_index[0],
-                    's': P1_model_trained[0],
-                    'm': P1_model_trained[1]
+                    'acc': P1_model_trained[0],
+                    'f1': P1_model_trained[1],
+                    'precision': P1_model_trained[2],
+                    'recall': P1_model_trained[3],
+                    'm': P1_model_trained[4]
                 },
                 {
                     'p': P2,
                     'i': sel_index[1],
-                    's': P2_model_trained[0],
-                    'm': P2_model_trained[1]
+                    'acc': P2_model_trained[0],
+                    'f1': P2_model_trained[1],
+                    'precision': P2_model_trained[2],
+                    'recall': P2_model_trained[3],
+                    'm': P2_model_trained[4]
                 },
                 {
                     'p': P3,
                     'i': sel_index[2],
-                    's': P3_model_trained[0],
-                    'm': P3_model_trained[1]
+                    'acc': P3_model_trained[0],
+                    'f1': P3_model_trained[1],
+                    'precision': P3_model_trained[2],
+                    'recall': P3_model_trained[3],
+                    'm': P3_model_trained[4]
                 }
             ]
 
             # Sort models based on fitness scores in descending order (higher is better, adjust if necessary)
-            sorted_fitness = sorted(fitness_scores, key=lambda x: x['s'], reverse=True)
+            sorted_fitness = sorted(fitness_scores, key=lambda x: x['f1'], reverse=True)
 
             # Get best, second best, and worst performing models
             best = sorted_fitness[0]['p']  # Best
@@ -353,7 +337,11 @@ class GA_assymetric_autoencoder:
             
 
             best_architecture.append(sorted_fitness[0])
-            print('Best result in generation', sorted_fitness[0]['s'])
+            print('Best result in generation (f1)', sorted_fitness[0]['f1'] )
+            print('Best result in generation (acc)',  sorted_fitness[0]['acc'])
+            
+            
+           
 
             population[sorted_fitness[1]['i']] = best_child
             population[sorted_fitness[2]['i']] = second_best_child
@@ -363,7 +351,7 @@ class GA_assymetric_autoencoder:
     
     def save_best_model_and_architecture(self, results):
     # Find the result with the best score
-        best_result = max(results, key=lambda x: x['s'])
+        best_result = max(results, key=lambda x: x['f1'])
         print(best_result)
         # Save the Keras model
         model = best_result['m']
@@ -377,6 +365,9 @@ class GA_assymetric_autoencoder:
             sys.stdout = f
             try:
                 self.print_architecture(architecture)
+                print('Best result in generation (f1)', best_result['f1'] )
+                print('Best result in generation (acc)',  best_result['acc'])
+            
             finally:
                 # Restore stdout
                 sys.stdout = original_stdout
